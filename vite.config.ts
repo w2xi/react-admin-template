069b981bc002d43bec477f91cf1ef031009add7a
w2xi <43wangxi@gmail.com>
2024-09-25 00:06:05 +0800

    chore: multi-language switch

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,8 @@ export default defineConfig({
       '@': '/src'
     }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+  ],
 })
