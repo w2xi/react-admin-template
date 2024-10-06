c4f6581ec827f8fabd6afe6b8f52a70da5ca3e1b
w2xi <43wangxi@gmail.com>
2024-10-06 11:30:50 +0800

    build: adjust base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV ? '/' : '/react-admin-template/',
+  base: process.env.NODE_ENV === 'production' ? '/react-admin-template/' : '/',
   resolve: {
     alias: {
       '@': '/src',
